cc92d34dfae040607f686858f84ff7344caef6a0
Farhan Mahmood <farhanmahmoodny@gmail.com>
2019-01-14 07:17:56 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -11,7 +11,19 @@ def monopoly
 end
 
 def monopoly_with_second_tier
-	monopoly = {:railroads => {:pieces => 4, :names => {}}}
+	monopoly = {:railroads => {
+	  :pieces => 4,
+	  :names => {
+	    :reading_railroad => {},
+	    :pennsylvania_railroad => {}
+	  },
+	  :rent_in_dollars => {
+	    :one_piece_owned => 25,
+	    :two_pieces_owned => 50,
+	    :three_pieces_owned => 100,
+	    :four_pieces_owned => 200
+	    
+	  }}}
 end
 
 def monopoly_with_third_tier
